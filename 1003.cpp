1b88d8763f500c69a876bc0f793c35e9c98176a9
wyr8845 <wyr527572059@qq.com>
2016-05-25 23:17:08 +0800

    Max Sum
    Given a sequence a[1],a[2],a[3]......a[n], your job is to calculate the
    max sum of a sub-sequence. For example, given (6,-1,5,4,-7), the max sum
    in this sequence is 6 + (-1) + 5 + 4 = 14.

---
+++ 1003.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+
+using namespace std;
+int main() {
+	int T;
+	int a;
+	int b;
+	int sum;
+	int max;
+	int start;
+	int end;
+	int lo;
+
+	cin >> T;
+
+	if (T <= 0) {
+		return 0;
+	}
+
+	for (int i = 0; i < T; i++) {
+		cin >> a;
+
+		for (int j = 1; j <= a; j++) {
+			cin >> b;
+
+			if (j == 1) {
+				max = sum = b;
+				lo = start = end = j;
+			} else {
+				if (b + sum < b) {
+					sum = b;
+					lo = j;
+				} else {
+					sum += b;
+				}
+			}
+
+			if (sum > max) {
+				max = sum;
+				end = j;
+				start = lo;
+			}
+		}
+
+		cout << "Case " << i + 1 << ":" << endl;
+
+		if (i < T - 1) {
+			cout << max << " " << start << " " << end << endl << endl;
+		} else {
+			cout << max << " " << start << " " << end << endl;
+		}
+	}
+
+	return 0;
+}
