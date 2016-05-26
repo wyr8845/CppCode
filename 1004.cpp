866d1654be230fc4a94a27cef805da7c9102a2b4
wyr8845 <wyr527572059@qq.com>
2016-05-26 09:00:00 +0800

    Let the Balloon Rise
    Contest time again! How excited it is to see balloons floating around.
    But to tell you a secret, the judges' favorite time is guessing the most
    popular problem. When the contest is over, they will count the balloons
    of each color and find the result.
    
    This year, they decide to leave this lovely job to you.

---
+++ 1004.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+int main() {
+	string str[1000];
+	int num[1000];
+	int N;
+	int max;
+	int x;
+
+	while(1) {
+		max = 1;
+		x = 0;
+
+		cin >> N;
+
+		if (N <= 0 || N > 1000) {
+			break;
+		}
+
+		for (int i = 0; i < N; i++) {
+			cin >> str[i];
+			num[i] = 1;
+		}
+
+		for (int i = 0; i < N - 1; i++) {
+			if (num[i] == 0) {
+				continue;
+			}
+
+			for (int j = i + 1; j < N; j++) {
+				if (num[j] == 0) {
+					continue;
+				}
+
+				if (str[i] == str[j]) {
+					num[i]++;
+					num[j]--;
+
+					if (num[i] > max) {
+						x = i;
+						max = num[i];
+					}
+				}
+			}
+		}
+
+		cout << str[x] << endl;
+	}
+
+	return 0;
+}
