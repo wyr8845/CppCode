7a60f7567c5c7b90baf1c5255e44d350aaad95de
wyr8845 <wyr527572059@qq.com>
2016-07-03 14:57:23 +0800

    排序

---
+++ 1040.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+
+using namespace std;
+int main() {
+	int T;
+	int N;
+	int a[1000];
+	int temp;
+
+	cin >> T;
+
+	for (int i = 0; i < T; i++) {
+		cin >> N;
+
+		for (int j = 0; j < N; j++) {
+			cin >> a[j];
+		}
+
+		for (int x = 0; x < N - 1; x++) {
+			for (int y = x + 1; y < N; y++) {
+				if (a[x] > a[y]) {
+					temp = a[x];
+					a[x] = a[y];
+					a[y] = temp;
+				}
+			}
+		}
+		
+		for (int k = 0; k < N - 1; k++) {
+			cout << a[k] << " ";
+		}
+
+		cout << a[N - 1] << endl;
+	}
+
+	return 0;
+}
