8c1973c6d6a957bcca7bb961a891793b9762ffa3
王逸然 <wyr527572059@qq.com>
2016-08-01 21:18:59 +0800

    将功赎过
    题目描述
    小赛是一名幸运的程序员。
    虽然他成功帮助小朋友以最快时间夺回了狼堡，但是面试官却打算和他说拜拜了。
    理由是——游戏天赋太高，有不务正业、走火入魔的倾向QAQ……
    尽管小赛很不能接受这个理由，可是却只能心灰意冷地吃下这个结果。
    然而，在他即将走出门的时候，面试官给了幸运的小赛一个最后的机会。
    原来，面试官的手机被他调皮的儿子小明用一个数字作为密码锁上了。
    小明只记得这个数字的十进制范围是l~r，且这个数的二进制表示中恰有m个1，却不记得确切的数字了。
    面试官可急坏了。这才有了小赛一个将功赎过的机会。
    他想要让小赛算出，他最坏情况下，要试多少次密码才能确保打开手机呢？
    请输出这个次数。

---
+++ 将功赎过.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+//主程序
+int main() {
+	int l, r, m;
+
+	while (cin >> l >> r >> m) {
+		int sum = 0;
+		for (int i = l; i <= r; i++) {
+			int j = i, count = 0;
+			while (j > 0) {
+				if (j & 1) {
+					count++;
+				}
+				j >>= 1;
+			}
+
+			if (count == m) {
+				sum++;
+			}
+		}
+
+		if (sum > 0) {
+			cout << sum << endl;
+		} else {
+			cout << -1 << endl;
+		}
+	}
+
+	return 0;
+}
