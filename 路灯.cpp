202b238e6571676d892c76e38a2b6ac770615b47
王逸然 <wyr527572059@qq.com>
2016-08-03 23:12:40 +0800

    路灯
    题目描述
    
    V先生有一天工作到很晚，回家的时候要穿过一条长l的笔直的街道，这条街道上有n个路灯。假设这条街起点为0，终点为l，第i个路灯坐标为ai。路灯发光能力以正数d来衡量，其中d表示路灯能够照亮的街道上的点与路灯的最远距离，所有路灯发光能力相同。为了让V先生看清回家的路，路灯必须照亮整条街道，又为了节省电力希望找到最小的d是多少？

---
+++ 路灯.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <string>
+#include <iomanip>
+#include <algorithm>
+#include <functional>
+using namespace std;
+
+//主程序
+int main() {
+	cout << setiosflags(ios::fixed) << setprecision(2);
+	int n;
+	int l;
+	int a[1000];
+
+	while (cin >> n >> l) {
+		for (int i = 0; i < n; i++) {
+			cin >> a[i];
+		}
+
+		sort(a, a + n, greater<int>());
+		
+		double d = 0;
+
+		for (int i = 0; i < n - 1; i++) {
+			d = max(d, (a[i] - a[i + 1]) / 2.0);
+		}
+
+		d = max(d, (l - a[0]) / 1.0);
+		d = max(d, a[n - 1] / 1.0);
+
+		cout << d << endl;
+	}
+
+	return 0;
+}
