97b50bda5dcf73f22e3f0b233b25df4d77ef9cf1
王逸然 <wyr527572059@qq.com>
2016-08-03 21:13:10 +0800

    计算器的新功能
    题目描述
    
    当你学一些可视化程序设计语言时，老师经常会让你设计并且编程做出一个计算器，这时也许你会仿照windows系统自带的计算器外观和功能去设计，但是现在老师要你多做出一个有新功能的计算器，实现当输入一个数时，能够将这个数分解成一个或多个素因子乘积的形式，并按素因子的大小排列显示出来。大家对计算器中数的表示应该很清楚的。下面显示出了0
    — 9这十个数字的表示形式。每个数字都占据5 * 3大小的字符区域

---
+++ 计算器的新功能.cpp
@@ -0,0 +1,165 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+char ch[5][1000];
+int pos;
+
+//数字变成5*3的样子
+void change(int num) {
+	switch (num) {
+	case 0:
+		ch[0][pos] = ' '; ch[0][pos + 1] = '-'; ch[0][pos + 2] = ' ';
+		ch[1][pos] = '|'; ch[1][pos + 1] = ' '; ch[1][pos + 2] = '|';
+		ch[2][pos] = ' '; ch[2][pos + 1] = ' '; ch[2][pos + 2] = ' ';
+		ch[3][pos] = '|'; ch[3][pos + 1] = ' '; ch[3][pos + 2] = '|';
+		ch[4][pos] = ' '; ch[4][pos + 1] = '-'; ch[4][pos + 2] = ' ';
+		pos = pos + 3;
+		break;
+	case 1:
+		ch[0][pos] = ' '; ch[0][pos + 1] = ' '; ch[0][pos + 2] = ' ';
+		ch[1][pos] = ' '; ch[1][pos + 1] = ' '; ch[1][pos + 2] = '|';
+		ch[2][pos] = ' '; ch[2][pos + 1] = ' '; ch[2][pos + 2] = ' ';
+		ch[3][pos] = ' '; ch[3][pos + 1] = ' '; ch[3][pos + 2] = '|';
+		ch[4][pos] = ' '; ch[4][pos + 1] = ' '; ch[4][pos + 2] = ' ';
+		pos = pos + 3;
+		break;
+	case 2:
+		ch[0][pos] = ' '; ch[0][pos + 1] = '-'; ch[0][pos + 2] = ' ';
+		ch[1][pos] = ' '; ch[1][pos + 1] = ' '; ch[1][pos + 2] = '|';
+		ch[2][pos] = ' '; ch[2][pos + 1] = '-'; ch[2][pos + 2] = ' ';
+		ch[3][pos] = '|'; ch[3][pos + 1] = ' '; ch[3][pos + 2] = ' ';
+		ch[4][pos] = ' '; ch[4][pos + 1] = '-'; ch[4][pos + 2] = ' ';
+		pos = pos + 3;
+		break;
+	case 3:
+		ch[0][pos] = ' '; ch[0][pos + 1] = '-'; ch[0][pos + 2] = ' ';
+		ch[1][pos] = ' '; ch[1][pos + 1] = ' '; ch[1][pos + 2] = '|';
+		ch[2][pos] = ' '; ch[2][pos + 1] = '-'; ch[2][pos + 2] = ' ';
+		ch[3][pos] = ' '; ch[3][pos + 1] = ' '; ch[3][pos + 2] = '|';
+		ch[4][pos] = ' '; ch[4][pos + 1] = '-'; ch[4][pos + 2] = ' ';
+		pos = pos + 3;
+		break;
+	case 4:
+		ch[0][pos] = ' '; ch[0][pos + 1] = ' '; ch[0][pos + 2] = ' ';
+		ch[1][pos] = '|'; ch[1][pos + 1] = ' '; ch[1][pos + 2] = '|';
+		ch[2][pos] = ' '; ch[2][pos + 1] = '-'; ch[2][pos + 2] = ' ';
+		ch[3][pos] = ' '; ch[3][pos + 1] = ' '; ch[3][pos + 2] = '|';
+		ch[4][pos] = ' '; ch[4][pos + 1] = ' '; ch[4][pos + 2] = ' ';
+		pos = pos + 3;
+		break;
+	case 5:
+		ch[0][pos] = ' '; ch[0][pos + 1] = '-'; ch[0][pos + 2] = ' ';
+		ch[1][pos] = '|'; ch[1][pos + 1] = ' '; ch[1][pos + 2] = ' ';
+		ch[2][pos] = ' '; ch[2][pos + 1] = '-'; ch[2][pos + 2] = ' ';
+		ch[3][pos] = ' '; ch[3][pos + 1] = ' '; ch[3][pos + 2] = '|';
+		ch[4][pos] = ' '; ch[4][pos + 1] = '-'; ch[4][pos + 2] = ' ';
+		pos = pos + 3;
+		break;
+	case 6:
+		ch[0][pos] = ' '; ch[0][pos + 1] = '-'; ch[0][pos + 2] = ' ';
+		ch[1][pos] = '|'; ch[1][pos + 1] = ' '; ch[1][pos + 2] = ' ';
+		ch[2][pos] = ' '; ch[2][pos + 1] = '-'; ch[2][pos + 2] = ' ';
+		ch[3][pos] = '|'; ch[3][pos + 1] = ' '; ch[3][pos + 2] = '|';
+		ch[4][pos] = ' '; ch[4][pos + 1] = '-'; ch[4][pos + 2] = ' ';
+		pos = pos + 3;
+		break;
+	case 7:
+		ch[0][pos] = ' '; ch[0][pos + 1] = '-'; ch[0][pos + 2] = ' ';
+		ch[1][pos] = ' '; ch[1][pos + 1] = ' '; ch[1][pos + 2] = '|';
+		ch[2][pos] = ' '; ch[2][pos + 1] = ' '; ch[2][pos + 2] = ' ';
+		ch[3][pos] = ' '; ch[3][pos + 1] = ' '; ch[3][pos + 2] = '|';
+		ch[4][pos] = ' '; ch[4][pos + 1] = ' '; ch[4][pos + 2] = ' ';
+		pos = pos + 3;
+		break;
+	case 8:
+		ch[0][pos] = ' '; ch[0][pos + 1] = '-'; ch[0][pos + 2] = ' ';
+		ch[1][pos] = '|'; ch[1][pos + 1] = ' '; ch[1][pos + 2] = '|';
+		ch[2][pos] = ' '; ch[2][pos + 1] = '-'; ch[2][pos + 2] = ' ';
+		ch[3][pos] = '|'; ch[3][pos + 1] = ' '; ch[3][pos + 2] = '|';
+		ch[4][pos] = ' '; ch[4][pos + 1] = '-'; ch[4][pos + 2] = ' ';
+		pos = pos + 3;
+		break;
+	case 9:
+		ch[0][pos] = ' '; ch[0][pos + 1] = '-'; ch[0][pos + 2] = ' ';
+		ch[1][pos] = '|'; ch[1][pos + 1] = ' '; ch[1][pos + 2] = '|';
+		ch[2][pos] = ' '; ch[2][pos + 1] = '-'; ch[2][pos + 2] = ' ';
+		ch[3][pos] = ' '; ch[3][pos + 1] = ' '; ch[3][pos + 2] = '|';
+		ch[4][pos] = ' '; ch[4][pos + 1] = '-'; ch[4][pos + 2] = ' ';
+		pos = pos + 3;
+		break;
+	default:
+		break;
+	}
+}
+
+//设置乘号*
+void multiSign() {
+	ch[0][pos] = ' ';
+	ch[1][pos] = ' ';
+	ch[2][pos] = '*';
+	ch[3][pos] = ' ';
+	ch[4][pos] = ' ';
+	pos = pos + 1;
+}
+
+//设置终结\0
+void endSign() {
+	ch[0][pos] = '\0';
+	ch[1][pos] = '\0';
+	ch[2][pos] = '\0';
+	ch[3][pos] = '\0';
+	ch[4][pos] = '\0';
+}
+
+//分解
+void decomposition(int num) {
+	bool flag = false;
+
+	for (int i = 2; num > 1;) {
+		if (num % i == 0) {
+			if (flag) {
+				multiSign();
+			} else {
+				flag = true;
+			}
+
+			int temp = i;
+			int j = 1;
+
+			while (temp / j / 10 != 0) {
+				j *= 10;
+			}
+
+			for (; j >= 1; j /= 10) {
+				change(temp / j);
+				temp %= j;
+			}
+			
+			num = num / i;
+		} else {
+			i++;
+		}
+	}
+	endSign();
+}
+
+//输出最终变形数字
+void print() {
+	for (int i = 0; i < 5; i++) {
+		cout << ch[i] << endl;
+	}
+}
+
+//主程序
+int main() {
+	int num;
+	
+	while (cin >> num) {
+		pos = 0;
+		decomposition(num);
+		print();
+	}
+
+	return 0;
+}
